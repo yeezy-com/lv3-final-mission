f7085e439e65f486db5d2ec03d40fc2a10de3a3b
GiSeok Kang <ime.rokz@gmail.com>
2025-06-10 16:48:04 +0900

    feat: 전역 예외 처리 로직 추가

---
+++ src/main/java/finalmission/music/global/handler/ErrorResponse.java
@@ -0,0 +1,4 @@
+package finalmission.music.global.handler;
+
+public record ErrorResponse(String message) {
+}
---
+++ src/main/java/finalmission/music/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package finalmission.music.global.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+}

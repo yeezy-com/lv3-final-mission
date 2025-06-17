fba983f796cd4a1600d651f38831a91de9605821
GiSeok Kang <ime.rokz@gmail.com>
2025-06-17 18:08:19 +0900

    chore: 예약 의존성 변경에 따라 sql문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,10 @@ VALUES ('Graduation', 'Kanye west', 14, '2007-12-04', 'WEQWFAFA');
 INSERT INTO Album (name, artist_name, total_tracks, release_date, spotify_id)
 VALUES ('DONDA', 'Kanye west', 21, '2012-12-04', 'WEQWF23112312AFA');
 
-INSERT INTO Reservation (album_id, member_name, address) VALUES (1, 'test1', '서울시 관악구');
-INSERT INTO Reservation (album_id, member_name, address) VALUES (2, 'test1', '서울시 관악구');
-INSERT INTO Reservation (album_id, member_name, address) VALUES (2, 'test2', '서울시 송파구');
+INSERT INTO Lottery (album_id, expiration_date_time) VALUES (1, '2025-06-17');
+INSERT INTO Lottery (album_id, expiration_date_time) VALUES (2, '2025-06-14');
+
+INSERT INTO Reservation (lottery_id, member_name, address) VALUES (1, 'test1', '서울시 관악구');
+INSERT INTO Reservation (lottery_id, member_name, address) VALUES (2, 'test1', '서울시 관악구');
+INSERT INTO Reservation (lottery_id, member_name, address) VALUES (2, 'test2', '서울시 송파구');
 

2fa84684d3b73408c7447b9a83db6c527111f92e
GiSeok Kang <ime.rokz@gmail.com>
2025-06-18 13:15:38 +0900

    feat: 글로벌 예외 핸들러 확장
    - IllegalStateException 및 Exception 처리 메서드 추가
    - 예외 로깅 추가

--- src/main/java/finalmission/music/global/handler/GlobalExceptionHandler.java
+++ src/main/java/finalmission/music/global/handler/GlobalExceptionHandler.java
@@ -1,14 +1,29 @@
 package finalmission.music.global.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.info("{} {}", e.getClass(), e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
+        log.info("{} {}", e.getClass(), e.getMessage());
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("{} {} {}", e.getClass(), e.getMessage(), e.getStackTrace());
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 }
